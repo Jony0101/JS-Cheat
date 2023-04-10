34b914b2ed0d501cc1a45d53027ee411fa11f601
Jony0101 <121823452+Jony0101@users.noreply.github.com>
2023-04-10 16:56:02 +0100

    Create Eternity.meta.js

---
+++ Files/Eternity.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Eternity
+// @version      3.3
+// @description  Tanki Online Cheat
+// @author       Akz
+// @match        https://*.test-eu.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+
+// @downloadURL  user
+// @updateURL    meta
+
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+
+
+// ==/UserScript==
